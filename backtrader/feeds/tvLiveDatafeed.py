4186680d34f94a816c524dd82f97b083aaa04db4
Ron Freimann <ronfreimann@gmail.com>
2022-07-09 10:12:30 +0300

    instead of flag check if new data in queue or not

--- backtrader/feeds/tvLiveDatafeed.py
+++ backtrader/feeds/tvLiveDatafeed.py
@@ -1,6 +1,9 @@
 '''
-TODO:
+CONTINUE:
+Study how Cerebro works with differnt data sets and how to access those different data sets
 
+Each live feed is a separate data object - for multiple assets(intervals) we need to create multiple separate live feeds and feed them into cerebro.
+The live feed object itself should not change, but the startegyRunner should change to create separate livefeeds based on asset_ids
 '''
 
 import queue
@@ -10,7 +13,6 @@ from backtrader.feeds import DataBase
 class tvLiveDatafeed(DataBase): # PARENT class is AbstractDataBase
 
     def __init__(self, tv_datafeed_realtime, asset_id):
-        self.newData = False # flag to indicate if data in queue available; set by receive_data and cleared by _load only when queue is empty
         self.tdr=tv_datafeed_realtime
         self.callback_id=self.tdr.add_callback(asset_id, self.receive_data) # add callback method for this asset set; keep callback id so later we can remove it
         self.data_queue=queue.Queue() # queue where all the new data is put for the _load() method to process
@@ -20,13 +22,12 @@ class tvLiveDatafeed(DataBase): # PARENT class is AbstractDataBase
 
     def receive_data(self, data): # this function is called by the tvDatafeedRealtime whenever the symbol we use in this datafeed has a new value
         self.data_queue.put(data[1]) # put new data in to queue and ignore the asset_id part
-        self.newData=True # set the flag so _load() knows there is new data
     
     def remove_callback(self):
         self.tdr.del_callback(self.callback_id)
 
     def _load(self):  # this is called inside the load() method in AbstractDataBase and will actually assign data to lines
-        if self.newData:
+        if not self.data_queue.empty(): # if there is new data in the queue
             data=self.data_queue.get() # get next available data bar
             if isinstance(data,str): # if string is received then that is "EXIT"
                 return False # means user stopped this strategy
@@ -41,9 +42,6 @@ class tvLiveDatafeed(DataBase): # PARENT class is AbstractDataBase
             self.lines.volume[0] = data.volume[0]
             self.lines.openinterest[0] = 0 # there is no such data in tvDatafeed so keep it zero
             
-            # check if queue is now empty (last data sample for now)
-            if self.data_queue.empty():
-                self.newData=False
             return True
         else:
             return None
